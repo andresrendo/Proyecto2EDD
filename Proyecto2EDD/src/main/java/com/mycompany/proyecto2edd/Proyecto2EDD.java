971013317e0ac7819812457b2006305c613588bc
AndresGabrielGG <97445082+AndresGabrielGG@users.noreply.github.com>
2022-11-27 17:10:29 -0400

    Update Proyecto2EDD.java

--- Proyecto2EDD/src/main/java/com/mycompany/proyecto2edd/Proyecto2EDD.java
+++ Proyecto2EDD/src/main/java/com/mycompany/proyecto2edd/Proyecto2EDD.java
@@ -4,6 +4,7 @@
  */
 
 package com.mycompany.proyecto2edd;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import com.mycompany.proyecto2edd.folder.ArbolGrafico;
@@ -15,15 +16,16 @@ public class Proyecto2EDD {
 
     public static void main(String[] args) {
 //        new ArbolBinarioExpresionesVentana().setVisible(true);
-          new Interfaz().setVisible(true);
-          SwingUtilities.invokeLater(new Runnable(){
-            @Override
-            public void run(){
-                JFrame frame = new ArbolGrafico();
-                frame.setSize(488,488);
-                frame.setVisible(true);
-            }
-          
-          });
+          Interfaz v1 =  new Interfaz();
+          v1.setVisible(true);
+//          SwingUtilities.invokeLater(new Runnable(){
+//            @Override
+//            public void run(){
+//                JFrame frame = new ArbolGrafico(v1);
+//                frame.setSize(488,488);
+//                frame.setVisible(true);
+//            }
+//          
+//          });
     }
 }
